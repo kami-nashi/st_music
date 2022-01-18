d2b9e9328757e2ed2ea36f71d0b9f7c44da0c16f
Ashley Young <asyoung@redhat.com>
2022-01-17 21:22:04 -0500

    updating gitignore and adding mainlogic

---
+++ app/lib/logicMain.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+    def warning(self, msg):
+        pass
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+def musicDownload(url):
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': '/tmp/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        print(info_dict)
+        #print("Title is", info_dict.get("title", None))
+        ydl.download([url])
+
+    return "Complete"
